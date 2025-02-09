9665bb123c80b97df367501733a249a68abb12c3
joaquin <jo.perezu@duocuc.cl>
2025-02-09 20:18:32 -0300

    aa

--- BDBookd/appBook/views.py
+++ BDBookd/appBook/views.py
@@ -9,7 +9,7 @@ from django.contrib.auth.models import User
 
 
 class BookView(viewsets.ModelViewSet):
-    queryset = models.Books.objects.all()
+    queryset = models.Book.objects.all()
     serializer_class = serializer.BookSerializer
 
     # cuando se hace una consulta get se ejecuta esta funcion
@@ -28,7 +28,7 @@ class BookView(viewsets.ModelViewSet):
                     status=status.HTTP_500_INTERNAL_SERVER_ERROR,
                 )
 
-            book, created = models.Books.objects.get_or_create(
+            book, created = models.Book.objects.get_or_create(
                 id=book_data["id"],
                 defaults={
                     "title": book_data["volumeInfo"]["title"],
@@ -50,6 +50,85 @@ class BookView(viewsets.ModelViewSet):
 
 
 # ?searchBook=Harry+Potter+y+la+piedra+filosofal
+
+
+class MyReadingsView(viewsets.ModelViewSet):
+    queryset = models.MyReading.objects.all()
+    serializer_class = serializer.MyReadingSerializer
+
+
+# Crear un libro en mi colección
+@api_view(["POST"])
+def addMyReading(request):
+    # extraemos la informacion desde el metodo post
+    book_data = request.data["book"]
+    # validamos haber recibido correctamente los datos
+    if not book_data:
+        return Response(
+            {"error": "Datos incompletos"}, status=status.HTTP_400_BAD_REQUEST
+        )
+    user = User.objects.get(email=request.data["user"])
+    if not user:
+        return Response(
+            {"error": "Reintenta hacer login"}, status=status.HTTP_401_UNAUTHORIZED
+        )
+    # creamos o revisamos si existe en nuestra base de datos el libro
+    book, created = models.Book.objects.get_or_create(
+        id=book_data["id"],
+        defaults={
+            "title": book_data["title"],
+            "description": book_data["description"],
+            "author": book_data["author"],
+            "category": book_data["category"],
+            "imgLink": book_data["imgLink"],
+            "pages": book_data["pages"],
+        },
+    )
+
+    myReading = models.MyReading.objects.create(
+        book=book,
+        user=user,
+        state=request.data["myReading"]["state"],
+        currentPage=request.data["myReading"]["currentPage"],
+        calification=request.data["myReading"]["calification"],
+        comments=request.data["myReading"]["comments"],
+        startReading=request.data["myReading"]["startReading"],
+        finishReading=request.data["myReading"]["finishReading"],
+        bookType=request.data["myReading"]["bookType"],
+    )
+
+    """ print(book_data["title"]) """
+    # devolver una respuesta adecuada
+    return Response(
+        {"message": "Book added successfully"}, status=status.HTTP_201_CREATED
+    )
+
+
+# Metodo get para traer mis libros de mi colección
+@api_view(["GET"])
+def getMyReadings(request):
+    email = request.query_params.get("email")
+    """ Validamos la existencia del email en el parametro """
+    if not email:
+        return Response(
+            {"error": "Parametros no validos"}, status=status.HTTP_400_BAD_REQUEST
+        )
+    # creamos el usuario a partir del email, si no entrega un error 404
+    try:
+        user = User.objects.get(email=email)
+    except User.DoesNotExist:
+        return Response(
+            {"error": "Usuario no existe"}, status=status.HTTP_404_NOT_FOUND
+        )
+    except Exception as e:
+        return Response(
+            {"error": "Error desconocido"}, status=status.HTTP_404_NOT_FOUND
+        )
+
+    books = models.MyReading.objects.filter(user=user).select_related("book")
+    # SERIALIZAMOS(JSON) EL QUERY SET BOOK
+    serializerr = serializer.MyReadingSerializer(books, many=True)
+    return Response(serializerr.data, status=status.HTTP_200_OK)
 
 
 @api_view(["POST"])
